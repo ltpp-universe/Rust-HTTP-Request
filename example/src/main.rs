b5935d7054a4092f46095a32a19be0c090c265b9
尤雨东 <root@ltpp.vip>
2024-09-24 22:54:37 +0800

    feat:example

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,21 +2,37 @@ use ::request::r#type::r#type::HttpRequest;
 use request::{
     methods::methods::Methods, protocol::protocol::Protocol, r#type::r#type::HttpRequestBuilder,
 };
+use std::collections::HashMap;
 
-pub fn _request_builder() -> HttpRequest {
-    use std::collections::HashMap;
+pub fn _post_request_builder() -> HttpRequest {
+    let mut body = HashMap::new();
+    body.insert("code".to_owned(), "".to_owned());
+    body.insert("language".to_owned(), "rust".to_owned());
+    body.insert("testin".to_owned(), "".to_owned());
     let http = HttpRequestBuilder::new()
-        .set_methods(Methods::GET)
-        .set_url("https://git.ltpp.vip/root/rust-http-request/-/tree/master/src?ref_type=heads")
+        .set_methods(Methods::POST)
+        .set_url("https://code.ltpp.vip/")
         .set_protocol(Protocol::HTTP)
-        .set_body(&HashMap::new())
+        .set_body(&body)
         .set_header(&HashMap::new())
         .builder();
     http
 }
 
+pub fn _get_request_builder() -> HttpRequest {
+    let http = HttpRequestBuilder::new()
+        .set_methods(Methods::GET)
+        .set_url("https://git.ltpp.vip/root/rust-http-request/-/tree/master/src?ref_type=heads")
+        .set_protocol(Protocol::HTTP)
+        .builder();
+    http
+}
+
 fn main() {
-    let req: HttpRequest = _request_builder();
-    let res: Result<String, request::error::error::Error> = req.send();
-    println!("{:?}", res);
+    let get_req: HttpRequest = _get_request_builder();
+    let get_res: Result<String, request::error::error::Error> = get_req.send();
+    println!("{:?}", get_res);
+    let post_req: HttpRequest = _get_request_builder();
+    let post_res: Result<String, request::error::error::Error> = post_req.send();
+    println!("{:?}", post_res);
 }
